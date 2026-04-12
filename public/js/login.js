8cd8e3a10a499e92095dcc44a8d03499f443ac89
Stienaat <frank.steenhoudt@gmail.com>
2026-04-12 20:17:44 +0200

    Add files via upload

--- public/js/login.js
+++ public/js/login.js
@@ -225,8 +225,9 @@ document.getElementById("loginForm")?.addEventListener("submit", async e => {
     });
 
     const data = await res.json();
-     showModal("success", "Welkom", "Je bent geregistreerd. ✔️ Je kunt nu inloggen !");
 	 window.location.href = "leden.html";
+     showModal("success", "Welkom", "Je bent geregistreerd. ✔️ Je kunt nu inloggen !");
+	
 
 
   }
